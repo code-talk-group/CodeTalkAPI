6e19d82786835d0260a66b1664cc041fba68b3b5
richard0923 <r.rosadoocasio@gmail.com>
2019-07-09 09:33:24 -0700

    added a couple of files

---
+++ CodeTalkAPI/CodeTalkAPI/Data/CodeTalkDBContext.cs
@@ -0,0 +1,19 @@
+﻿using CodeTalkAPI.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CodeTalkAPI.Data
+{
+    public class CodeTalkDBContext : DbContext
+    {
+        public CodeTalkDBContext(DbContextOptions<CodeTalkDBContext> options) : base(options) { }
+        
+        
+        public DbSet<CodeModel> DefaultSnippets { get; set; }
+
+        public DbSet<CodeModel> UserSnippets { get; set; }
+    }
+}
--- CodeTalkAPI/CodeTalkAPI/Startup.cs
+++ CodeTalkAPI/CodeTalkAPI/Startup.cs
@@ -27,8 +27,8 @@ namespace CodeTalkAPI
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
-            services.AddDbContext<CodeTalkDBContext>(opt => 
-                opt.UseSqlServer()) //Richard - need to add the connection string for the DataBase  
+            //services.AddDbContext<CodeTalkDBContext>(opt => 
+            //    opt.UseSqlServer()) //Richard - need to add the connection string for the DataBase  
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
