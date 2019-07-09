bdbf6acb01fe033aa2ec1375d422940d2d717fcd
Meggan <meggtriplett@gmail.com>
2019-07-09 14:27:10 -0700

    Added HTTP get to return a default record

---
+++ CodeTalkAPI/CodeTalkAPI/Classes/Greeting.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CodeTalkAPI.Classes
+{
+    public class Greeting
+    {
+        public string GreetingMessage { get; set; }
+    }
+}
--- CodeTalkAPI/CodeTalkAPI/Controllers/DefaultController.cs
+++ CodeTalkAPI/CodeTalkAPI/Controllers/DefaultController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using CodeTalkAPI.Data;
+using CodeTalkAPI.Models;
+using CodeTalkAPI.Classes;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
@@ -12,6 +14,20 @@ namespace CodeTalkAPI.Controllers
     [ApiController]
     public class DefaultController : ControllerBase
     {
-       
+        private CodeTalkDBContext _context;
+
+        public DefaultController(CodeTalkDBContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet ("{options:int}", Name = "Get")]
+        public Default Get(int option)
+        {
+            var defaultObject = _context.DefaultSnippets.Find(option);
+            return defaultObject;
+        }
+
+        
     }
 }
