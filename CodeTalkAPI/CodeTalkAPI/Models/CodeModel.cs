757d90eeb573df277fd3f54cd247b7eb3ea96413
richard0923 <r.rosadoocasio@gmail.com>
2019-07-09 09:05:14 -0700

    Added project

---
+++ CodeTalkAPI/CodeTalkAPI/Models/CodeModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CodeTalkAPI.Models
+{
+    public class CodeModel
+    {
+        //Richard - Need to add properties for code model's
+    }
+}
---
+++ CodeTalkAPI/CodeTalkAPI/Models/CodeTalkDBContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CodeTalkAPI.Models
+{
+    public class CodeTalkDBContext : DbContext
+    {
+        public CodeTalkDBContext(DbContextOptions<CodeTalkDBContext> options)
+            : base(options)
+        {
+        }
+
+        //Richard - Needs the DBSet for all the tables 
+
+    }
+}
