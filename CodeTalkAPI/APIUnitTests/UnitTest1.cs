a3775f056b1aebb422b1c55e8f0962656bcebfaa
Meggan <meggtriplett@gmail.com>
2019-07-10 11:20:52 -0700

    Added UnitTest file

---
+++ CodeTalkAPI/APIUnitTests/UnitTest1.cs
@@ -0,0 +1,16 @@
+using System;
+using Xunit;
+using System.Collections.Generic;
+using CodeTalkAPI.Classes;
+
+namespace ClassMethodUnitTests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void CanCreateSpokenCodeString()
+        {
+            
+        }
+    }
+}
