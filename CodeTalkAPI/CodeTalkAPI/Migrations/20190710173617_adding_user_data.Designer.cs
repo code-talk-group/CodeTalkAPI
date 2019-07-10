a28315cc29ae94cb18efc623efe18315d50b95de
richard0923 <r.rosadoocasio@gmail.com>
2019-07-10 11:56:11 -0700

    adding Http actions to users

--- CodeTalkAPI/CodeTalkAPI/Migrations/20190710173617_adding_user_data.Designer.cs
+++ CodeTalkAPI/CodeTalkAPI/Migrations/20190710173617_adding_user_data.Designer.cs
@@ -3,14 +3,16 @@ using CodeTalkAPI.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace CodeTalkAPI.Migrations
 {
     [DbContext(typeof(CodeTalkDBContext))]
-    partial class CodeTalkDBContextModelSnapshot : ModelSnapshot
+    [Migration("20190710173617_adding_user_data")]
+    partial class adding_user_data
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
