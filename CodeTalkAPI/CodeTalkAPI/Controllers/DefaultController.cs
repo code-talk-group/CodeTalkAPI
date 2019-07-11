7199aa500a1bfb79b3b949e23f4619772e26996c
Meggan <meggtriplett@gmail.com>
2019-07-11 13:33:48 -0700

    added userObject for postuser param

--- CodeTalkAPI/CodeTalkAPI/Controllers/DefaultController.cs
+++ CodeTalkAPI/CodeTalkAPI/Controllers/DefaultController.cs
@@ -66,7 +66,7 @@ namespace CodeTalkAPI.Controllers
                 Input = inputsString
             };
 
-            var returnObject = await UserController.PostUser();
+            var returnObject = await UserController.PostUser(userObject);
             return Ok(returnObject);
         }
     }
