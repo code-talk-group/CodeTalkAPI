31b5edbaba3a71ffa7c017ab4bb5927a2c7cadbf
richard0923 <r.rosadoocasio@gmail.com>
2019-07-11 17:48:00 -0700

    change sentences

--- CodeTalkAPI/CodeTalkAPI/Data/CodeTalkDBContext.cs
+++ CodeTalkAPI/CodeTalkAPI/Data/CodeTalkDBContext.cs
@@ -24,19 +24,19 @@ namespace CodeTalkAPI.Data
                 new Default
                 {
                     Id = 2,
-                    BaseString = "",
+                    BaseString = "MethodName is a public method which takes in an integer array called ArrayName and returns an integer. A counter is declared and set to zero. A For Loop iterates through the array as long as i is less than the length of the array and adds 1 to the counter. When the loop is completed the counter is returned.",
                     Options = Options.For_Loop
                 },
                 new Default
                 {
                     Id = 3,
-                    BaseString = "_ is a public method with a void return type that takes in an integer named _ . The integer's value is then set to _. Our if statement determines if _ is less than 10. If this is true, Yes is printed to the console. If this is not true, our else statement will print No to the console.",
+                    BaseString = "MethodName is a public method with a void return type that takes in an integer named pName . The integer's value is then set to userInt. Our if statement determines if pName is less than 10. If this is true, Yes is printed to the console. If this is not true, our else statement will print No to the console.",
                     Options = Options.If_Statement
                 },
                 new Default
                 {
                     Id = 4,
-                    BaseString = "_ is a public method with a void return type. A _ variable called _ is declared and set to equal _.",
+                    BaseString = "MethodName is a public method with a void return type. A dataType variable called varName is declared and set to equal userData.",
                     Options = Options.Variable
                 }
                 );
