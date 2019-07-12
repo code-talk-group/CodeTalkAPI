864f0eb3fb6bc1da0541a2b44f8fa35913cb3ef3
richard0923 <r.rosadoocasio@gmail.com>
2019-07-12 09:05:47 -0700

    pulling from dev

--- CodeTalkAPI/CodeTalkAPI/Data/CodeTalkDBContext.cs
+++ CodeTalkAPI/CodeTalkAPI/Data/CodeTalkDBContext.cs
@@ -18,25 +18,25 @@ namespace CodeTalkAPI.Data
                 new Default
                 {
                     Id = 1,
-                    BaseString = "MethodName is a public method with a void return type that takes in a UserDataType called ParamName. When the method is called all the statements and arguments defined within the curly braces will run.",
+                    BaseString = "METHODNAME is a public method with a void return type that takes in a  USERDATATYPE called PARAMNAME. When the method is called all the statements and arguments defined within the curly braces will run.",
                     Options = Options.Function
                 },
                 new Default
                 {
                     Id = 2,
-                    BaseString = "MethodName is a public method which takes in an integer array called ArrayName and returns an integer. A counter is declared and set to zero. A For Loop iterates through the array as long as i is less than the length of the array and adds 1 to the counter. When the loop is completed the counter is returned.",
+                    BaseString = "METHODNAME is a public method which takes in an integer array called ARRAYNAME and returns an integer. A counter is declared and set to zero. A For Loop iterates through the array as long as i is less than the length of the array and adds 1 to the counter. When the loop is completed the counter is returned.",
                     Options = Options.For_Loop
                 },
                 new Default
                 {
                     Id = 3,
-                    BaseString = "MethodName is a public method with a void return type that takes in an integer named pName. The integer's value is then set to userInt. Our if statement determines if pName is less than 10. If this is true, Yes is printed to the console. If this is not true, our else statement will print No to the console.",
+                    BaseString = "METHODNAME is a public method with a void return type that takes in an integer named PNAME. The integer's value is then set to USERINT. Our if statement determines if PNAME is less than 10. If this is true, Yes is printed to the console. If this is not true, our else statement will print No to the console.",
                     Options = Options.If_Statement
                 },
                 new Default
                 {
                     Id = 4,
-                    BaseString = "MethodName is a public method with a void return type. A dataType variable called varName is declared and set to equal userData.",
+                    BaseString = "METHODNAME is a public method with a void return type. A DATATYPE variable called VARNAME is declared and set to equal USERDATA.",
                     Options = Options.Variable
                 }
                 );
---
+++ CodeTalkAPI/CodeTalkAPI/Migrations/20190712034309_newwr.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CodeTalkAPI.Migrations
+{
+    public partial class newwr : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
